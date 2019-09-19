87a61f37d80f85ccf2c3704590a32536de832a6d
40823246 <40823246.gm.nfu.edu.tw>
2019-09-20 01:09:58 +0800

    initial add

---
+++ content/tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'https://www.youtube.com/?gl=TW&hl=zh-tw \n', 'tags': '', 'url': 'About.html'}, {'title': 'usb', 'text': '', 'tags': '', 'url': 'usb.html'}]};
