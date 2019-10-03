a3bc99cf96efb97a3dc023eeff42c6456efc2ccf
40823246 <40823246.gm.nfu.edu.tw>
2019-10-03 14:10:34 +0800

    add embedding dartpad

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'First', 'text': '\n About \n https://www.youtube.com/?gl=TW&hl=zh-tw \n \n 3d 列印.pdf \n', 'tags': '', 'url': 'First.html'}, {'title': 'usb', 'text': '', 'tags': '', 'url': 'usb.html'}]};
+var tipuesearch = {"pages": [{'title': 'First', 'text': 'https://dartpad.dev?id=82be62c70020d1e99250b6fa8078ba36\xa0 \n \n About \n https://www.youtube.com/?gl=TW&hl=zh-tw \n \n 3d 列印.pdf \n', 'tags': '', 'url': 'First.html'}, {'title': 'usb', 'text': '', 'tags': '', 'url': 'usb.html'}]};
