c4b326562b6708e9719ecf5840308664e7a8c312
40823246 <40823246.gm.nfu.edu.tw>
2019-11-21 23:04:55 +0800

    add w11

--- downloads/dart_ex/for/main.dart
+++ downloads/dart_ex/for/main.dart
@@ -1,9 +1,7 @@
 void main(){
-  int i=0;
-  int sum=0;
-  for(i;i<20;i++){
-    sum += i;
-  print('$sum');
+  for(int i=0;i<10;i++)
+  {
+    print('hi ${i+i}');
   }
-  print('sum=$sum');
+  print('hi hi hi');
 }
